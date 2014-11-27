ed09543d64b2533b94555bc5937ae4be5ab8f7fe
Josef Adersberger <josef.adersberger@qaware.de>
2014-11-27 14:37:46 +0100

    Korrektur: Vorlage und Loesung

--- U06-GCE/Vorlage/src/main/java/edu/qaware/cc/jclouds/LaunchNginxCluster.java
+++ U06-GCE/Vorlage/src/main/java/edu/qaware/cc/jclouds/LaunchNginxCluster.java
@@ -22,18 +22,21 @@ public class LaunchNginxCluster {
         //(2) Eine Instanz in der Compute Cloud launchen
         NodeMetadata node = launch("europe-west1-c", 
                                          "europe-west1-c/g1-small", 
-                                         "centos-7-v20141108",
-                                         "vl-cc", cs);
+        //********************************************************************
+        //TODO: Folgend ein aktuelles CentOS Image wählen
+        //********************************************************************
+                                         "<<TODO>>", 
+                                         "vl-cc", cs);        
 
         //(3) SSH Kommandos ausführen
         exec(node.getId(), "sudo yum -y install docker", cs);
-        exec(node.getId(), "sudo service docker start", cs);
-        exec(node.getId(), "sudo docker build -t cloudcomputing/nginx-node http://goo.gl/Ye6l2s", cs);
-        exec(node.getId(), "sudo docker build -t cloudcomputing/haproxy-node http://goo.gl/pGkdfl", cs);
-        for (int i = 1; i <= 3; i++) {
-            exec(node.getId(), "sudo docker run -d -p 8" + i + ":80 --name nginx" + i + " cloudcomputing/nginx-node", cs);
-        }
-        exec(node.getId(), "sudo docker run -d -p 80:80 --link nginx1:nginx1 --link nginx2:nginx2 --link nginx3:nginx3 cloudcomputing/haproxy-node", cs);
+        exec(node.getId(), "sudo service docker start", cs);   
+        
+        //********************************************************************
+        //TODO: Kommandos entsprechend der Übung zur Provisionierung absetzen
+        //Hinweis: Allen Kommandos muss "sudo" vorgestellt werden, 
+        //damit diese privilegiert ausgeführt werden können
+        //********************************************************************
         
         //(4) Daten zum erzeugten Knoten ausgeben
         System.out.println("Knoten erzeugt: " + node.getId());        
