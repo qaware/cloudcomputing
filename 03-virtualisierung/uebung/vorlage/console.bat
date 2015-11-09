36b19a126215ad41a6d37c4c47b54ddb8b5ffe45
Josef Adersberger <josef.adersberger@qaware.de>
2015-11-09 10:16:42 +0100

    Vorlesungsteil zur Virtualisierung ergänzt

---
+++ 03-virtualisierung/uebung/vorlage/console.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ==================================================
+echo Vorlesung Cloud Computing - Uebung Virtualisierung
+echo ==================================================
+SET PATH=%PATH%;./ssh
+cmd
