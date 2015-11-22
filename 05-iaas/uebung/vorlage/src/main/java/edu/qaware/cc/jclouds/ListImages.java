e6a712938a7d9f54ef1459116849f7295a69ed9a
Josef Adersberger <josef.adersberger@qaware.de>
2015-11-22 19:32:27 +0100

    Revert "Revert "Modul 5: IaaS ergänzt in der initialen Version (Übung noch in Überarbeitung)""
    This reverts commit 361cd9c2711c43e98639409a27dd371148ce84d4.

---
+++ 05-iaas/uebung/vorlage/src/main/java/edu/qaware/cc/jclouds/ListImages.java
@@ -0,0 +1,24 @@
+package edu.qaware.cc.jclouds;
+
+import edu.qaware.cc.jclouds.utils.CloudUtils;
+import static edu.qaware.cc.jclouds.utils.CloudUtils.connect;
+import java.io.IOException;
+import org.jclouds.compute.ComputeService;
+
+/**
+ * Gibt die Liste der verfügbaren Images aus.
+ */
+public class ListImages {
+    public static void main(String[] args) throws IOException {        
+        ComputeService cs = connect(
+                "844448415254-61r7t0e6skchhm2hr8r33c2ni77v5gpk@developer.gserviceaccount.com",
+                "vl-cc.pem", "google-compute-engine").getComputeService();       
+        
+        //********************************************************************
+        //TODO
+        //********************************************************************
+        
+        cs.getContext().close();
+    }
+ 
+}
