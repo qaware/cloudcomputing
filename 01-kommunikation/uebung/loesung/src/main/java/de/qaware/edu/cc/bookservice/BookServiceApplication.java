5f4fedaf93984ed0f6aaf0dfda1a4f17f7a1d564
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-10-15 22:05:46 +0200

    Added Sample books service.

---
+++ 01-kommunikation/uebung/loesung/src/main/java/de/qaware/edu/cc/bookservice/BookServiceApplication.java
@@ -0,0 +1,12 @@
+package de.qaware.edu.cc.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+}
