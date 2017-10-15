5aa451d28f1d07a881586591d4ea728b411da192
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-10-15 22:26:02 +0200

    Added CQRS filter.

---
+++ 01-kommunikation/uebung/loesung/src/main/java/de/qaware/edu/cc/bookservice/CQRSFilter.java
@@ -0,0 +1,21 @@
+package de.qaware.edu.cc.bookservice;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CQRSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MultivaluedMap<String, String> headers = requestContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
