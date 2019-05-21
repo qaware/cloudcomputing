f926950048bd33b9aca6385ec80b7fdecac8f84e
Moritz Kammerer <moritz.kammerer@qaware.de>
2019-05-21 16:16:31 +0200

    Re-Implement in Spring Web MVC
    Consul & Jersey use JAX-RS 1 and 2. This clashes on the classpath. I therefore removed JAX-RS 2 and reimplemented the logic in Spring Web MVC.

--- 08-cloud-architektur/uebung/book-service/src/main/java/de/qaware/edu/cc/bookservice/BookServiceApplication.java
+++ 08-cloud-architektur/uebung/book-service/src/main/java/de/qaware/edu/cc/bookservice/BookServiceApplication.java
@@ -2,11 +2,13 @@ package de.qaware.edu.cc.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The main Spring boot application class.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class BookServiceApplication {
 
     public static void main(String[] args) {
