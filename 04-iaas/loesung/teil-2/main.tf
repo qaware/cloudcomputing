f3db1b632021a664414674eee4b28eeb361334c8
Bernhard Saumweber <bernhard.saumweber@qaware.de>
2023-05-03 11:14:01 +0200

    Update provider declaration for newer terraform versions

--- 04-iaas/loesung/teil-2/main.tf
+++ 04-iaas/loesung/teil-2/main.tf
@@ -1,10 +1,19 @@
-provider aws {
-  version = "3.12.0"
-  region  = "us-east-1"
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "< 4.0"
+    }
+  }
 }
 
-provider random {
-  version = "3.0.0"
+provider aws {
+  region  = "us-east-1"
 }
 
 resource random_string id_suffix {
